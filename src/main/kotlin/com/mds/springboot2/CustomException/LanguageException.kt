e7bc4acdd2cbe2e93e638877e68836fec53dc185
Ronan Lachater <ronan.lachater@gmail.com>
2017-11-06 22:17:44 +0100

    starting to play with kotlin..

--- src/main/kotlin/com/mds/springboot2/CustomException/LanguageException.kt
+++ src/main/kotlin/com/mds/springboot2/CustomException/LanguageException.kt
@@ -3,3 +3,6 @@ package com.mds.springboot2.CustomException
 /**
  * Created by ronan on 06/11/17.
  */
+class LanguageException(override var message: String?): Exception(message){
+
+}
--- src/main/kotlin/com/mds/springboot2/notes/Language.kt
+++ src/main/kotlin/com/mds/springboot2/notes/Language.kt
@@ -1,5 +1,7 @@
 package com.mds.springboot2.notes
 
+import com.mds.springboot2.CustomException.LanguageException
+
 /**
  * Created by ronan on 06/11/17.
  * Enum of Languages used in our app
@@ -13,5 +15,15 @@ enum class Language {
 
     companion object {
 
+    }
+    /*
+     * singleton factory
+     */
+    object LowerLanguageFactory {
+        fun createLanguageLower(str: String?):String? {
+            if(str == "french") return Language.FRENCH.name.toLowerCase()
+            else if(str == "english") return Language.ENGLISH.name.toLowerCase()
+            else throw LanguageException("Wrong choice off language")
+        }
     }
 }
