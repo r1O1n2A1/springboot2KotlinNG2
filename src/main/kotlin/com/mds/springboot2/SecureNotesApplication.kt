3ac7baa3e8b8aed1027dcaa1b7aceab8da644e8b
Ronan Lachater <ronan.lachater@gmail.com>
2017-11-12 11:58:58 +0100

    Adding kafka + vaadin features, config ok

--- src/main/kotlin/com/mds/springboot2/SecureNotesApplication.kt
+++ src/main/kotlin/com/mds/springboot2/SecureNotesApplication.kt
@@ -2,8 +2,10 @@ package com.mds.springboot2
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.kafka.annotation.EnableKafka
 
 @SpringBootApplication
+@EnableKafka
 class SecureNotesApplication
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/com/mds/springboot2/ui/ChatDisplay.kt
@@ -0,0 +1,12 @@
+package com.mds.springboot2.ui
+
+import com.vaadin.*;
+import com.vaadin.ui.Panel
+
+/**
+ * Class which display chat messages
+ * Created by ronan on 12/11/17.
+ */
+class ChatDisplay:Panel() {
+
+}
